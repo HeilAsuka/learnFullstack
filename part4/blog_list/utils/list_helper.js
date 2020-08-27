9bdeeb2e799d15636bbcea4a042ab7b2a03fa1ed
XiuQiang Shang <heilasuka911@gmail.com>
2020-08-27 10:22:18 +0800

    4.5 complete

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -4,10 +4,18 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     if (blogs.length === 0) return 0;
     const reducer = (total, currentBlog) => total + currentBlog.likes;
-    return blogs.reduce(reducer,0)
+    return blogs.reduce(reducer, 0);
 };
-
+const favoriteBlog = (blogs) => {
+    const sorted = blogs.sort((a, b) => b.likes - a.likes);
+    return {
+        title: sorted[0].title,
+        author: sorted[0].author,
+        likes: sorted[0].likes,
+    };
+};
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
 };
